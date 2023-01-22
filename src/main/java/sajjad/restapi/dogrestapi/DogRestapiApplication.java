47ba55a2e5782b16b6b9d6d5a398829da3faeb53
SajjadSedaqat <Sajjad.Sedaqat@Gmail.com>
2023-01-22 20:07:38 +0330

    Initial commit

---
+++ src/main/java/sajjad/restapi/dogrestapi/DogRestapiApplication.java
@@ -0,0 +1,13 @@
+package sajjad.restapi.dogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sajjad/restapi/dogrestapi/DogRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sajjad.restapi.dogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

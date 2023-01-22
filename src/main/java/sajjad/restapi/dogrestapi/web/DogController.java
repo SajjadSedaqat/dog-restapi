1c78c7068edec77e77745dde73934f45e4ffe79a
SajjadSedaqat <Sajjad.Sedaqat@Gmail.com>
2023-01-23 01:51:49 +0330

    Update DogController to implement queries

--- src/main/java/sajjad/restapi/dogrestapi/web/DogController.java
+++ src/main/java/sajjad/restapi/dogrestapi/web/DogController.java
@@ -1,8 +1,49 @@
 package sajjad.restapi.dogrestapi.web;
 
 
+import org.apache.juli.logging.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import sajjad.restapi.dogrestapi.entity.Dog;
+import sajjad.restapi.dogrestapi.service.DogService;
+
+import java.util.List;
 
 @RestController
 public class DogController {
+
+    private final DogService dogService;
+
+    @Autowired
+    public DogController(DogService dogService) {
+        this.dogService = dogService;
+    }
+
+    @GetMapping("/dogs")
+    public ResponseEntity<List<Dog>> getAllDogs() {
+        List<Dog> dogs = dogService.retrieveDogs();
+        return new ResponseEntity<>(dogs, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/breed")
+    public ResponseEntity<List<String>> getDogBreeds() {
+        List<String> breedList = dogService.retrieveDogBreed();
+        return new ResponseEntity<>(breedList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/breed")
+    public ResponseEntity<String> getBreedById(@PathVariable Long id){
+        String breed = dogService.retrieveDogBreedById(id);
+        return new ResponseEntity<>(breed, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/name")
+    public ResponseEntity<List<String>> getDogNames() {
+        List<String> list = dogService.retrieveDogNames();
+        return new ResponseEntity<List<String>>(list, HttpStatus.OK);
+    }
 }
